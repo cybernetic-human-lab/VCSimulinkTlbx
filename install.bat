0376c4b0931f0d44a47b34b707388dd00035c70e
Ulysses Bernardet <bernuly@gmail.com>
2016-06-16 17:16:05 -0700

    adding install scripts

---
+++ install.bat
@@ -0,0 +1,16 @@
+@echo OFF
+set LIB_NAME=VCSimulinkTlbx
+set USER_MATLAB_DIR=%USERPROFILE%\Documents\MATLAB
+set INSTALL_DIR=%USER_MATLAB_DIR%\%LIB_NAME%
+mkdir %INSTALL_DIR%
+
+echo copying files to %INSTALL_DIR%
+REM pause
+
+copy VCTlbx.slx     %INSTALL_DIR%
+copy slblocks.m        %INSTALL_DIR%
+copy pawnPos.mexw32    %INSTALL_DIR%
+copy gazeString.mexw32 %INSTALL_DIR%
+copy faceString.mexw32 %INSTALL_DIR%
+copy AUsMaleBML.mexw32   %INSTALL_DIR%
+copy AUsFemaleBML.mexw32 %INSTALL_DIR%
---
+++ install_64bit.bat
@@ -0,0 +1,17 @@
+@echo OFF
+set LIB_NAME=VCSimulinkTlbx
+set USER_MATLAB_DIR=%USERPROFILE%\Documents\MATLAB
+set INSTALL_DIR=%USER_MATLAB_DIR%\%LIB_NAME%
+mkdir %INSTALL_DIR%
+
+echo copying files to %INSTALL_DIR%
+REM pause
+
+copy VCTlbx.slx     %INSTALL_DIR%
+copy slblocks.m        %INSTALL_DIR%
+copy pawnPos.mexw64    %INSTALL_DIR%
+copy gazeString.mexw64 %INSTALL_DIR%
+copy faceString.mexw64 %INSTALL_DIR%
+copy AUsMaleBML.mexw64   %INSTALL_DIR%
+copy AUsFemaleBML.mexw64 %INSTALL_DIR%
+
